76a3da45247e89910057b02af6282152f9ff9994
saypython <285210769@qq.com>
2017-09-08 17:22:57 +0800

    Initial commit

---
+++ pages/test/test.js
@@ -0,0 +1,9 @@
+Page({
+  onPullDownRefresh: function () {
+    console.log('1111')
+    setTimeout(wx.stopPullDownRefresh,500)
+  },
+  onLoad: function (options){
+    wx.startPullDownRefresh()
+  }
+})
