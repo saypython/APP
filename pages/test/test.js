65990e43306dc4cbfe3f0db37e406f96ff94849a
saypython <285210769@qq.com>
2017-09-08 17:25:42 +0800

    Revert "Initial commit"
    This reverts commit 76a3da45247e89910057b02af6282152f9ff9994.

--- pages/test/test.js
+++
@@ -1,9 +0,0 @@
-Page({
-  onPullDownRefresh: function () {
-    console.log('1111')
-    setTimeout(wx.stopPullDownRefresh,500)
-  },
-  onLoad: function (options){
-    wx.startPullDownRefresh()
-  }
-})
